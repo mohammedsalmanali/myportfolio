c1e8c8e156bd593d310d96c6b8f2e54056f38b90
mohammedsalmanali <salmanmohammed2098@gmail.com>
2022-08-26 16:43:30 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
-
+import React from 'react';
 
 function App() {
   return (
